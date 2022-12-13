a8cd1b5e0e7aed0ed835ee6df150a6ba36e10152
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2022-12-13 09:43:24 +0700

    feat: multiple emacs profile, emacs dir, and more.

--- .local/share/emacsdir/fainemacs/init.el
+++ .local/share/emacsdir/fainemacs/init.el
@@ -228,3 +228,16 @@
   :ensure t
   :pin melpa-stable
   :config (evil-collection-init))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-collection evil ox-gfm visual-fill-column org-bullets markdown-mode yaml-mode magit counsel-projectile projectile which-key counsel ivy rainbow-delimiters doom-themes doom-modeline all-the-icons use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
